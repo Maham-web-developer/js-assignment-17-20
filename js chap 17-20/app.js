6e785604e7ebbfa77382ca524df1a42bbb391028
Maham web developer <125392502+Maham-web-developer@users.noreply.github.com>
2023-06-16 20:23:33 +0500

    Add files via upload

---
+++ js chap 17-20/app.js
@@ -0,0 +1,119 @@
+// ===========Chapter 17-20===========
+
+// Question 1
+// var multidimensionalArray = [];
+
+// multidimensionalArray.push([]);
+// multidimensionalArray.push([]);
+
+// Question 2
+// var matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+//   ];
+
+//   Question 3
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+
+//   Question 4
+// var number = parseInt(prompt("Enter the number for multiplication table:"));
+// var length = parseInt(prompt("Enter the length of the multiplication table:"));
+
+// console.log(`Multiplication Table of ${number} up to ${length}:`);
+// for (let i = 1; i <= length; i++) {
+//   var result = number * i;
+//   console.log(`${number} * ${i} = ${result}`);
+// }
+
+// Question 5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// console.log("Items of the fruits array:");
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// Question 6
+
+// console.log("Counting:");
+// for (let i = 1; i <= 15; i++) {
+//   console.log(i);
+// }
+
+// console.log("Reverse counting:");
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// console.log("Even:");
+// for (let i = 0; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+// console.log("Odd:");
+// for (let i = 1; i <= 19; i += 2) {
+//   console.log(i);
+// }
+
+// console.log("Series:");
+// for (let i = 2; i <= 20; i += 2) {
+//   console.log(i + "k");
+// }
+
+// Question 7
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var userInput = prompt("Enter an item to search in the list:");
+
+// let found = false;
+// for (let i = 0; i < A.length; i++) {
+//   if (A[i] === userInput) {
+//     found = true;
+//     break;
+//   }
+// }
+// if (found) {
+//   alert("cookie is available in our bakery");
+// } else {
+//   alert("We are sorry.pastry is not available in our bakery");
+// }
+
+// Question 8
+// var numbers = [10, 5, 20, 8, 15];
+// var largestNumber = numbers[0]; 
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > largestNumber) {
+//     largestNumber = numbers[i];
+//   }
+// }
+// console.log("The largest number is:", largestNumber);
+
+// Question 9
+// var A = [24, 53, 78, 91, 12];
+// var smallestNumber = A[0];
+
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] < smallestNumber) {
+//     smallestNumber = A[i];
+//   }
+// }
+// console.log("The smallest number is:", smallestNumber);
+
+// Question 10
+// console.log("Multiples of 5 from 1 to 100:");
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+
+
+
+  
+
+
